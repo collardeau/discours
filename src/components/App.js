1286711576a6fafd1b71000e800662d8f16f2000
Thomas Collardeau <thomas@collardeau.com>
2015-07-11 17:17:02 +0200

    create a sample state at top of App

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,29 @@ import React from 'react';
 
 export default class App extends React.Component {
 
+  constructor(){
+    super();
+    this.state = {
+      ancestor: 'root',
+      content: 'Who will win?',
+      resp: [{
+        content: 'Federer',
+        count: '5',
+        ref: 'some id'
+      }, {
+        content: 'Djokovic',
+        count: '4',
+        ref: 'some id'
+      }]
+    };
+  }
+
   render(){
 
-    console.log("new react component");
     return (
       <div>
         <h1>Hello, world</h1>
+        <p>{ this.state.content}</p>
       </div>
     );
   }
