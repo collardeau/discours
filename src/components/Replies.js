f234cb91d849874bc7558d86f897d377cd545579
Thomas Collardeau <thomas@collardeau.com>
2015-08-05 18:02:33 +0200

    start with redux

--- src/components/Replies.js
+++ src/components/Replies.js
@@ -16,7 +16,7 @@ export default class Replies extends React.Component {-}
 
     return (
       <div>
-        <Filter parentKey={this.props.parentKey}/>
+        <Filter topicKey={this.props.topicKey}/>
         <ul>{replies()}</ul>
       </div>
     );
