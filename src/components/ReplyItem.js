22cc475fe5e8338c4b5937fe86837c267f43e988
Thomas Collardeau <thomas@collardeau.com>
2015-08-07 19:11:15 +0200

    fix upvote accordingly

--- src/components/ReplyItem.js
+++ src/components/ReplyItem.js
@@ -8,8 +8,8 @@ export default class Item extends Component {-}
     return nextProps.reply !== this.props.reply;
 
   handleButtonClick = () => {-}
-    let { key, topicKey } = this.props.reply;
-    upvote(key, topicKey);
+    let { key, topic } = this.props.reply;
+    upvote(key, topic.key);
 
   render(){-}
 
