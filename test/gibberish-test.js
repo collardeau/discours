095639c5fc602124a0783233b86104b28b170ab2
Thomas Collardeau <thomas@collardeau.com>
2015-08-19 15:44:12 +0200

    add test for gibberish input

---
+++ test/gibberish-test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var should = require("chai").should();
+
+describe("will test for gibberish", function () {
+
+  it("works", function () {
+    var input = 1;
+    input.should.equal(1);
+  });
+});
