06e8d0736b930c3df7f5b25c3b3d23b5c905768b
Thomas Collardeau <thomas@collardeau.com>
2015-08-25 17:40:27 +0200

    cleaner route redirects

--- src/actions/routeActions.js
+++ src/actions/routeActions.js
@@ -11,12 +11,8 @@ function selectRoute(route) {
 export function changeRoute(route){
   return (dispatch, getState) => {
 
-    console.log('changing route from this hash: ', route);
     let params = route.split('/'),
       entry = params.shift();
-
-    console.log('entry: ', entry);
-    console.log('params: ', entry);
 
     const prevTopicId = getState().selectedTopic;
     if(prevTopicId && params[0] !== prevTopicId){ // a different topic
@@ -24,15 +20,17 @@ export function changeRoute(route){
     }
 
     dispatch(selectRoute(entry));
-    console.log('changing route to :', entry);
 
-    if(!entry.length){
-      console.log('not entry so use new/root');
-      dispatch(fetchTopicAndReplies('new', 'root'));
+    if(!entry){
+      dispatch(changeRoute('new/root'));
     }
     
     if (entry === 'new' || entry === 'popular') {
-      dispatch(fetchTopicAndReplies(entry, params[0]));
+      if(!params[0]){
+        dispatch(changeRoute(entry + '/root'));
+      }else {
+        dispatch(fetchTopicAndReplies(entry, params[0]));
+      }
     }
 
   };
