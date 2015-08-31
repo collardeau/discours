3ab0cf5cdb69585012448a43df321cbef88742ee
Thomas Collardeau <thomas@collardeau.com>
2015-08-31 11:32:28 +0200

    fix typos

--- heroku.js
+++ heroku.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production') {
-  var child_proess = require('child_process');
+  var child_process = require('child_process');
   var proc = "webpack -p --config webpack.production.config.js";
-  return child_process.exec(proc), function(error, stdout, stderr) {
+  return child_process.exec(proc, function(error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null){
